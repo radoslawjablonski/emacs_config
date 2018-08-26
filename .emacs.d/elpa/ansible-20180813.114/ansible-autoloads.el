676cd6e3b5bea4cf91055aaa9fbfd468681a21b5
Radoslaw Jablonski <radoslaw.jablonski@hotmail.com>
2018-08-26 18:56:06 +0200

    Add downloaded elpa plugins
    For sources behing strict http proxy

---
+++ .emacs.d/elpa/ansible-20180813.114/ansible-autoloads.el
@@ -0,0 +1,35 @@
+;;; ansible-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ansible" "ansible.el" (23419 10288 564210
+;;;;;;  622000))
+;;; Generated autoloads from ansible.el
+
+(defvar ansible::key-map (make-sparse-keymap) "\
+Keymap for Ansible.")
+
+(autoload 'ansible "ansible" "\
+Ansible minor mode.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'ansible::dict-initialize "ansible" "\
+Initialize Ansible auto-complete.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ansible-pkg.el") (23419 10288 548209
+;;;;;;  809000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ansible-autoloads.el ends here
